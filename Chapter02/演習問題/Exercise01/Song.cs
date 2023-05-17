e0888fcec027514ab222b36f1006bbb4c7ec614e
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-05-17 11:21:35 +0900

    p.79演習問題

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //2.1.1
+    class Song {
+        //プロパティ
+        public string Title { get; set; }   //歌のタイトル    
+        public string ArtistName { get; set; }  //アーティスト名        
+        public int Length { get; set; } //演奏時間（単位：秒）
+
+        //2.1.2
+        //コンストラクタ
+        public Song(string title, string artistName, int length) {
+            Title = title;
+            ArtistName = artistName;
+            Length = length;
+        }
+    }
+}
