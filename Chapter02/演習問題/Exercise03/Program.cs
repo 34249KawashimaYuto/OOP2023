3ff6f52fcaeb325a9d510974215b5b823418bbf6
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-05-22 10:22:46 +0900

    2-3 I/F変更

--- Chapter02/演習問題/Exercise03/Program.cs
+++ Chapter02/演習問題/Exercise03/Program.cs
@@ -8,15 +8,20 @@ using System.Threading.Tasks;
 namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("**売上集計**");
+            Console.WriteLine("1 店舗別売り上げ");
+            Console.WriteLine("2 商品カテゴリー別売り上げ");
+            Console.Write("＞ ");
+            string choice = Console.ReadLine();
             var sales = new SalesCounter(@"data\sales.csv");
-            //店舗別売上
-            //var amountPerStore = sales.GetPerStoreSales();
-            //foreach (KeyValuePair<string, int> obj in amountPerStore) {
-            //    Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
-            //}
-            //カテゴリー別売上
-            var amountPerCategory = sales.GetPerCategorySales();
-            foreach (KeyValuePair<string, int> obj in amountPerCategory) {
+            var amount = sales.GetPerStoreSales();//初期値として店舗別を設定
+            switch (int.Parse(choice)) {
+                case 2:
+                    //カテゴリー別売上
+                    amount = sales.GetPerCategorySales();                    
+                    break;
+            }
+            foreach (KeyValuePair<string, int> obj in amount) {
                 Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
             }
         }
