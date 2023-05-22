2528390f66561e6f2219352d223a3495f75df46b
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-05-22 09:54:16 +0900

    2-3完成

--- Chapter02/演習問題/Exercise03/Program.cs
+++ Chapter02/演習問題/Exercise03/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,17 @@ using System.Threading.Tasks;
 namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
+            var sales = new SalesCounter(@"data\sales.csv");
+            //店舗別売上
+            //var amountPerStore = sales.GetPerStoreSales();
+            //foreach (KeyValuePair<string, int> obj in amountPerStore) {
+            //    Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
+            //}
+            //カテゴリー別売上
+            var amountPerCategory = sales.GetPerCategorySales();
+            foreach (KeyValuePair<string, int> obj in amountPerCategory) {
+                Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
+            }
         }
     }
 }
