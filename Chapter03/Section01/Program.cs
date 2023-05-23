87cee340a70cf175bedf17d3d0a84a08c49b6228
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-05-23 09:24:10 +0900

    p87 step5まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -8,19 +8,8 @@ namespace Section01 {
     class Program {
         static void Main(string[] args) {
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-            //Judgement judge = IsEven;
-            int count = Count(numbers, delegate (int n) { return n % 2 == 0; });
+            int count = numbers.Count(n => n % 2 == 0);
             Console.WriteLine(count);
-        }
-
-        static public int Count(int[] numbers, Predicate<int> judge) {     
-            int count = 0;
-            foreach (var n in numbers) {
-                if (judge(n) == true) {
-                    count++;
-                } 
-            }
-            return count;
         }
     }
 }
