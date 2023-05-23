d7a7c0c6cd45ab87b56947327540e3fa2559a74c
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-05-23 11:12:08 +0900

    p90まで

--- Chapter03/Section03/Program.cs
+++ Chapter03/Section03/Program.cs
@@ -8,8 +8,15 @@ namespace Section03 {
     class Program {
         static void Main(string[] args) {
             var list = new List<string> {
-               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+               "Tokyo", "New Delhi", "Bangkok", "London",
+               "Paris", "Berlin", "Canberra", "Hong Kong",
             };
+
+            //var exists = list.Exists(s => s[0] == 'c');
+            //Console.WriteLine(exists);
+
+            var names = list.ConvertAll(s => s.ToLower());
+            names.ForEach(s => Console.WriteLine(s));
         }
     }
 }
