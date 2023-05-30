4877b63345c62cbbae1231396c63bbf5540de538
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-05-30 12:09:23 +0900

    演習問題　動作確認

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -8,11 +8,12 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
-            var ym = new YearMonth(2023, 5);
+            var ym = new YearMonth(2023, 12);
             var c21 = ym.Is21Century;
             var ymNextMonth = ym.AddOneMonth();
 
             Console.WriteLine(ym);
+            Console.WriteLine(ymNextMonth);
         }
     }
 }
