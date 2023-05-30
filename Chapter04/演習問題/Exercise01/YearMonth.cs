13d4eb797f872b256a1eda4f7ba983daef7c7329
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-05-30 11:52:25 +0900

    演習問題4.1.3完成

--- Chapter04/演習問題/Exercise01/YearMonth.cs
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -15,5 +15,31 @@ namespace Exercise01 {
             Year = year;
             Month = month;
         }
+
+        //4.1.2
+        public bool Is21Century {
+            get {
+                return Year >= 2001 && Year <= 2100;
+            }
+        }
+
+        //4.1.3
+        public YearMonth AddOneMonth() {
+            var addOneYear = Year;
+            var addOneMonth = Month;
+            if(Month == 12) {
+                addOneYear++;
+                addOneMonth++;
+            }
+           else {
+                addOneMonth++;
+            }
+            return new YearMonth(addOneYear,addOneMonth);
+        }
+
+        //4.1.4
+        //public override string ToString() {
+            //return "{0}日{1}月,";
+        //}
     }
 }
--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Exercise01;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,9 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
+            var ym = new YearMonth(2023, 5);
+            var c21 = ym.Is21Century;
+            var ymNextMonth = ym.AddOneMonth();
         }
     }
 }
