792ba8af717d7d735045dfea6dcf9492cc3bc907
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-05-31 11:57:11 +0900

    演習問題5.2完成

---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("数値：");
+            int.TryParse(Console.ReadLine(),out int result);
+            var addComma = result.ToString("#,0");
+
+            Console.WriteLine(result);
+            Console.WriteLine(addComma);
+        }
+    }
+}
