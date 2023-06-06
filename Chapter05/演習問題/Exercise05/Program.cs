8df238559e77b3a09415d24340c5721322549002
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-06-06 11:03:38 +0900

    演習問題5_4 StringArray完成

--- Chapter05/演習問題/Exercise05/Program.cs
+++ Chapter05/演習問題/Exercise05/Program.cs
@@ -1,5 +1,5 @@
-﻿#define NonArray
-//#define StringArray
+﻿//#define NonArray
+#define StringArray
 
 using System;
 using System.Collections.Generic;
@@ -12,15 +12,7 @@ namespace Exercise04 {
         static void Main(string[] args) {
 #if NonArray
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-#elif StringArray
-            string[] lines = {
-                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
-                "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867",
-                "Novelist=太宰治;BestWork=人間失格;Born=1909",
-                "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
-            };
 
-#endif
             var sw = 0;
             foreach (var semicolon in line.Split(';')) {
                 foreach (var equal in semicolon.Split('=')) {
@@ -33,22 +25,46 @@ namespace Exercise04 {
                     sw++;
                 }
             }
+#elif StringArray
+            string[] lines = {
+                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+                "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867",
+                "Novelist=太宰治;BestWork=人間失格;Born=1909",
+                "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+            };
+
+            var sw = 1;
+            foreach (var array in lines) {
+                foreach (var semicolon in array.Split(';')) {
+                    foreach (var equal in semicolon.Split('=')) {
+                        if (sw % 2 != 0) {
+                            if (sw > 0) {
+                            }
+                            Console.Write("{0}：", Item(equal));
+                        }
+                        else {
+                            Console.WriteLine(equal);
+                            if (sw % 3 == 0) {
+                                Console.WriteLine();
+                            }
+                        }
+                        sw++;
+                    }
+                }
+            }
+#endif
         }
         //項目名を返すメソッド
         private static string Item(string item) {
-            string returnItem = "";
             switch (item) {
                 case "Novelist":
-                    returnItem = "作家　";
-                    break;
+                    return "作家　";
                 case "BestWork":
-                    returnItem = "代表作";
-                    break;
+                    return "代表作";
                 case "Born":
-                    returnItem = "誕生年";
-                    break;
+                    return "誕生年";
             }
-            return returnItem;
+            throw new ArgumentException("引数が正しくありません");
         }
     }
 }
