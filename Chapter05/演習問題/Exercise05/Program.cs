27a32c8c6f4f2047b33a5ed7d41b575e85109131
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-06-06 10:29:48 +0900

    演習問題5_4 NonArray完成

--- Chapter05/演習問題/Exercise05/Program.cs
+++ Chapter05/演習問題/Exercise05/Program.cs
@@ -1,13 +1,54 @@
-﻿using System;
+﻿#define NonArray
+//#define StringArray
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise05 {
+namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
+#if NonArray
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+#elif StringArray
+            string[] lines = {
+                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+                "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867",
+                "Novelist=太宰治;BestWork=人間失格;Born=1909",
+                "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+            };
+
+#endif
+            var sw = 0;
+            foreach (var item in line.Split(';')) {
+                foreach (var item2 in item.Split('=')) {
+                    if (sw % 2 == 0) {
+                        Console.Write("{0}：", Item(item2));
+                    }
+                    else {
+                        Console.WriteLine(item2);
+                    }
+                    sw++;
+                }
+            }
+        }
+        //項目名を返すメソッド
+        private static string Item(string item) {
+            string returnItem = "";
+            switch (item) {
+                case "Novelist":
+                    returnItem = "作家　";
+                    break;
+                case "BestWork":
+                    returnItem = "代表作";
+                    break;
+                case "Born":
+                    returnItem = "誕生年";
+                    break;
+            }
+            return returnItem;
         }
     }
 }
