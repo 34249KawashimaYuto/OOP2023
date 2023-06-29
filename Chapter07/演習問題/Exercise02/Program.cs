c741bac8b7eeaa3b451adabb716a9900ea1ec78d
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-06-29 12:09:11 +0900

    演習問題7.2.2まで

---
+++ Chapter07/演習問題/Exercise02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var abbrs = new Abbreviations();
+
+            //addメソッドの呼び出し例
+            abbrs.Add("IOP", "国際オリンピック委員会");
+            abbrs.Add("NPT", "核兵器不拡散条約");
+        }
+    }
+}
