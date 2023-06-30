29b713e24a1fd87373dde1381be34dd737b72205
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-06-30 11:38:45 +0900

    演習問題　8.1完成

---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            //var dateTime = new DateTime(2019, 1, 15, 19, 48, 32);
+            var dateTime = DateTime.Now;
+            DisplayDatePattern1(dateTime);
+            DisplayDatePattern2(dateTime);
+            DisplayDatePattern3(dateTime);
+            DisplayDatePattern3_2(dateTime);
+        }
+
+        private static void DisplayDatePattern1(DateTime dateTime) {
+            Console.WriteLine(dateTime.ToString("yyyy年M月d日 HH:mm"));
+        }
+
+        private static void DisplayDatePattern2(DateTime dateTime) {
+            Console.WriteLine(dateTime.ToString("yyyy年MM月dd日 HH時mm分ss秒"));
+        }
+
+        private static void DisplayDatePattern3(DateTime dateTime) {
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            Console.WriteLine(dateTime.ToString("ggyy年M月d日(dddd)", culture));
+        }
+
+        private static void DisplayDatePattern3_2(DateTime dateTime) {
+            
+        }
+    }
+}
