76b93189bf52b802dfefdf7e1366eb6b448da798
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-06-30 12:05:48 +0900

    演習問題　8.2途中

---
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var dateTime = DateTime.Today;
+            Console.WriteLine(NextDay(dateTime, DayOfWeek.Monday).ToString("yyyy年M月d日(ddd)"));
+        }
+
+        private static DateTime NextDay(DateTime dateTime, DayOfWeek dayOfWeek) {
+            var days = (int)dayOfWeek - (int)dateTime.DayOfWeek;    //指定曜日と本日の曜日の差を求める
+            return dateTime.AddDays(days + 7);
+        }
+    }
+}
