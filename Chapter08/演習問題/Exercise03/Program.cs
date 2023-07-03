19e9145d32829ac532408a50562f06ab04ec5f4e
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-07-03 13:44:55 +0900

    演習問題　8.3作成

---
+++ Chapter08/演習問題/Exercise03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(string[] args) {
+            var tw = new TimeWatch();
+            //tw.Start();
+            //Thread.Sleep(1000);
+            //TimeSpan duration = tw.Stop();
+            //Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalMilliseconds);
+        }
+    }
+
+    class TimeWatch {
+
+
+
+    }
+}
