eca13e030dd0d406464c283c3bd3e6213828ccba
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-09-13 11:57:47 +0900

    自動マイグレーション（p.343）

--- Chapter13/SampleEntityFramework/Program.cs
+++ Chapter13/SampleEntityFramework/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,11 +22,18 @@ namespace SampleEntityFramework {
             //UpdateBook();
             //DeleteBook();
 
-            foreach(var book in GetBooks()) {
-                Console.WriteLine($"{book.Title} {book.Author.Name}");
+            //foreach(var book in GetBooks()) {
+            //Console.WriteLine($"{book.Title} {book.Author.Name}");
+            //}
+
+            using (var db = new BooksDbContext()) {
+                db.Database.Log = sql => { Debug.Write(sql); };
+
+                var count = db.Books.Count();
+                Console.WriteLine(count);
             }
 
-            Console.WriteLine();
+                Console.WriteLine();
         }
         // List 13-5
         static void InsertBooks() {
