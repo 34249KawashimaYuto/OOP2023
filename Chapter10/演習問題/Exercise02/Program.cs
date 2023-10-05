8220c06117226d0da94fb718f40813582d5fa4fc
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-10-05 09:52:15 +0900

    演習問題2完成

---
+++ Chapter10/演習問題/Exercise02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            Pickup3DigitNumber("sample.txt");
+        }
+
+        private static void Pickup3DigitNumber(string file) {
+            foreach (var line in File.ReadLines(file)) {
+                var regex = new Regex(@"^\d{3}\d*$");
+                foreach (var words in line.Split(new char[] { ' ', }, StringSplitOptions.RemoveEmptyEntries)) {
+                    if (regex.IsMatch(words))
+                        Console.WriteLine(words);
+                }
+            }
+        }
+    }
+}
