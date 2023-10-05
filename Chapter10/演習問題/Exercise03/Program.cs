d6783cebf83fda0a8aadba6e8f5d7edd22376c4a
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-10-05 10:31:08 +0900

    演習問題3完成

---
+++ Chapter10/演習問題/Exercise03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(string[] args) {
+            var texts = new[] {
+               "Time is money.",
+               "What time is it?",
+               "It will take time.",
+               "We reorganized the timetable.",
+            };
+            foreach (var text in texts) {
+                var match = Regex.Match(text, @"[Tt]ime");
+                if (match.Success) {
+                    Console.WriteLine(text);
+                    Console.WriteLine("開始位置:{0}番目",match.Index + 1);
+                }
+            }
+
+
+
+        }
+    }
+}
