a90356672d8d4e6a5a5dbf69ebaeb71eff7901fd
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-10-10 09:49:07 +0900

    演習問題4完成

--- Chapter10/演習問題/Exercise04/Program.cs
+++ Chapter10/演習問題/Exercise04/Program.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
             var lines = File.ReadAllLines("sample.txt");
+            var replaced = lines.Select(s => Regex.Replace(s, @"\b[Vv]ersion( |)=( |)""v4.0""", @"varsion=""v5.0"""));
 
-
-
-
-
+            //書き込み
+            File.WriteAllLines("sample.txt", replaced);
 
             // これ以降は確認用
             var text = File.ReadAllText("sample.txt");
