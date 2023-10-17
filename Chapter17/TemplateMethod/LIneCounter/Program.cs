13284f6cd1714b7261f500a98465ed524a375c75
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-10-17 12:07:50 +0900

    lineCount

---
+++ Chapter17/TemplateMethod/LIneCounter/Program.cs
@@ -0,0 +1,15 @@
+﻿using LIneCounter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFileProcessor;
+
+namespace LineCounter {
+    class Program {
+        static void Main(string[] args) {
+            TextProcessor.Run<LineCounterProcessor>(@"C:\Users\infosys\Desktop\ソース.txt");
+        }
+    }
+}
