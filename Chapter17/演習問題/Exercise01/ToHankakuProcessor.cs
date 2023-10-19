2ecd33c9e4753265941a2d6bdefbe30eb43fda85
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-10-19 10:57:59 +0900

    演習問題01完成(自力)

---
+++ Chapter17/演習問題/Exercise01/ToHankakuProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class ToHankakuProcessor :TextProcessor{
+        private List<string> _line = new List<string>();
+        private static Dictionary<char, char> dictionary = new Dictionary<char, char>()
+        {
+            {'０','0'},{'１','1'},{'２','2'},{'３','3'},{'４','4'},{'５','5'},{'６','6'},{'７','7'},{'８','8'},{ '９','9'},
+        };
+
+        protected override void Execute(string line) {
+            var str = line;
+            foreach (var num in dictionary) {
+                str = Regex.Replace(str, num.Key.ToString(), num.Value.ToString());
+            }
+            _line.Add(str);
+        }
+        protected override void Terminate() {
+            foreach (var item in _line) {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
