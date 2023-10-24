8b6186cf3445dbd039da9c4e341c563d750b4a37
川島佑斗 <OIBC34249@ojs.ac.jp>
2023-10-24 11:55:07 +0900

    wpf第1回

--- Chapter17/演習問題/Exercise01/ToHankakuProcessor.cs
+++ Chapter17/演習問題/Exercise01/ToHankakuProcessor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Exercise01.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,21 +7,24 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise01 {
-    class ToHankakuProcessor :TextProcessor{
+    class ToHankakuProcessor :Framework.ITextFileService{
         private List<string> _line = new List<string>();
         private static Dictionary<char, char> dictionary = new Dictionary<char, char>()
         {
             {'０','0'},{'１','1'},{'２','2'},{'３','3'},{'４','4'},{'５','5'},{'６','6'},{'７','7'},{'８','8'},{ '９','9'},
         };
+        void ITextFileService.Initialize(string fname) {
+        }
 
-        protected override void Execute(string line) {
+        void ITextFileService.Execute(string line) {
             var str = line;
             foreach (var num in dictionary) {
                 str = Regex.Replace(str, num.Key.ToString(), num.Value.ToString());
             }
             _line.Add(str);
         }
-        protected override void Terminate() {
+
+        void ITextFileService.Terminate() {
             foreach (var item in _line) {
                 Console.WriteLine(item);
             }
